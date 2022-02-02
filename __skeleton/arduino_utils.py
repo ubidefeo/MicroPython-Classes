0ee1ac73fe1c31669ed878c6d2b78cf070e1ea08
ubi de feo <me@ubidefeo.com>
2022-02-02 12:12:35 +0100

    further cleanup

--- __skeleton/arduino_utils.py
+++ __skeleton/arduino_utils.py
@@ -1,33 +1,27 @@
 # Arduino Nano pins to RP2040 pins
-
 from machine import Pin
 
-class InvalidPin(Exception):
-    # Raised when user tries to initialise an invalid Pin
-    pass
+A0 = 14
+A1 = 15
+A2 = 16
+A3 = 17
+A4 = 18
+A5 = 19
+A6 = 20
+A7 = 21
 
-A0 = 'A0'
-A1 = 'A1'
-A2 = 'A2'
-A3 = 'A3'
-A4 = 'A4'
-A5 = 'A5'
-A6 = 'NA'
-A7 = 'NA'
-
-digital_pins = {0: 0, 1: 1, 2: 25, 3: 15, 4: 16, 5: 17, 6: 18, 7: 19, 8: 20, 9: 21, 10: 5, 11: 7, 12: 4, 13: 6}
-analog_pins = {'A0': 26, 'A1': 27, 'A2': 28, 'A3': 29, 'A4': 12, 'A5': 13}
-
-def nano_pin(pin_id):
-    try:
-        if(pin_id in digital_pins):
-            return digital_pins[pin_id]
-        if(pin_id in analog_pins):
-            return analog_pins[pin_id]
-        raise InvalidPin
-    except InvalidPin:
-        print('Pin not supported for the operation')
+board_pins = {0: 0, 1: 1, 2: 25, 3: 15, 4: 16, 5: 17, 6: 18, 7: 19, 8: 20, 9: 21, 10: 5, 11: 7, 12: 4, 13: 6, A0: 26, A1: 27, A2: 28, A3: 29, A4: 12, A5: 13, A6: -1, A7: -1}
 
 class ArduinoPin(Pin):
     def __init__(self, pin_id, mode, pull = None):
-        pass
+        hardware_pin = board_pins[pin_id]
+        self.pin = Pin(hardware_pin, mode, pull)
+
+    def on(self):
+        self.pin.on()
+
+    def off(self):
+        self.pin.off()
+
+    def toggle(self):
+        self.pin.toggle()
---
+++ __skeleton/boot.py
@@ -0,0 +1,2 @@
+from arduino_utils import *
+from arduino_utils import ArduinoPin as Pin
